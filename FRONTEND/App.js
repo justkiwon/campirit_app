4418d5266c57cfe6e52b7d84ada7a5168a6f9a8b
justkiwon <72431775+justkiwon@users.noreply.github.com>
2022-01-11 18:54:20 +0900

    Update App.js

--- FRONTEND/App.js
+++ FRONTEND/App.js
@@ -1,205 +1,96 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import {Picker} from '@react-native-picker/picker';
-import {TouchableOpacity,ScrollView,Text,Image, View, StyleSheet, SafeAreaView, Button, Dimensions,Alert} from 'react-native';
+import React, { useState,} from 'react';
+import { StyleSheet} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import MainPage from './screens/MainPage.js';
+import QuizStart from './screens/QuizPages/QuizStart.js';
+import QuizSituation from './screens/QuizPages/QuizSituation.js';
+import Quiz1 from './screens/QuizPages/Quiz1.js';
+import Quiz2 from './screens/QuizPages/Quiz2.js';
+import Quiz3 from './screens/QuizPages/Quiz3.js';
+import Quiz4 from './screens/QuizPages/Quiz4.js';
+import Quiz5 from './screens/QuizPages/Quiz5.js';
+import Quiz6 from './screens/QuizPages/Quiz6.js';
+import Quiz7 from './screens/QuizPages/Quiz7.js';
+import Quiz8 from './screens/QuizPages/Quiz8.js';
+import Quiz9 from './screens/QuizPages/Quiz9.js';
+import Quizf from './screens/QuizPages/Quizf.js';
+import MakeKit from './screens/MakeKit.js';
+import StartPage from './screens/StartPage.js';
 
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
+const Stack = createNativeStackNavigator();
 
-export default function StartPage({ ansList, route , navigation }) { 
-  const tent1='바람막이 텐트'
-  const tent2='바람막이 텐트'
-  const tent3='바람막이 텐트'
-  const [selectedLanguage, setSelectedLanguage] = useState();
+export default function App() {
+  const[ansList, setansList] = useState(
+    {
+      A1:"",
+      A2:"",
+      A3:"",
+      A4:0,
+      A5:0,
+      A6:0,
+      A7:0,
+      A8:0,
+      A9:0,
+      Af:0,
+    });
 
-
-  //========================================= Selete box function ========================================
-  const Selete_box = (props) =>{
-    return(
-    <View style={styles.select_box}>
-        <Image source={require("./assets/images/MakeKit/retangle.png")} style={styles.footer_selate_img}/>
-          <View style={{left:10}}>
-            <Text style={{fontSize:10}}>{props.tent_name}</Text>
-            <Text style={{fontSize:10}}>선택한 옵션: {props.value}</Text>
-  
-              <Picker selectedValue={selectedLanguage}
-                onValueChange={(itemValue, itemIndex) => setSelectedLanguage(itemValue)} 
-                  style={{width:185, height:0 , borderWidth: 2,borderStyle: 'solid'}}>
-                <Picker.Item label={props.option1} value="option1" style={styles.picker_value}/>
-                <Picker.Item label={props.option2} value="option2" style={styles.picker_value}/>
-                <Picker.Item label={props.option3} value="option3" style={styles.picker_value}/>
-              </Picker>
-  
-          </View>
-            <Text style={{marginTop:1,left:-50}}>{props.money}원</Text>
-      </View>
-    )};
-//============================================ Product_info =========================================================
-const Product_info = (props) =>{
-    return(
-  <ScrollView style={styles.Content_list} horizontal = {true}>  
-    <View style={styles.Content_explain}>
-        <Text style={styles.Content_title}>{props.name}</Text>
-          <Text style={styles.Content}>{props.info}</Text>
-    </View>    
-      <View style={{flexDirection: 'row'}} >
-       <Image source={require("./assets/images/MakeKit/slid_button2.png")} style={styles.slid_button}/>
-        <View style={styles.specific_item}>
-{/* ---------------------------------------------product 1-------------------------------------------- */}      
-        <View style={styles.products}>
-             <View style={{flexDirection: 'column'}}>
-             <Image source={require("./assets/images/MakeKit/retangle.png")}/> 
-              <Text>안녕</Text>  
-              <Text>3000원</Text> 
-            </View>         
-                <TouchableOpacity>
-                  <Image source={require("./assets/images/MakeKit/product_select.png")} style={{marginTop:windowHeight/8, left: -windowWidth/17}} />
-                </TouchableOpacity>
-            </View>  
-{/* ---------------------------------------------product 2-------------------------------------------- */}
-            <View style={styles.products}>
-             <View style={{flexDirection: 'column'}}>
-             <Image source={require("./assets/images/MakeKit/retangle.png")}/> 
-              <Text>안녕</Text>  
-              <Text>3000원</Text> 
-            </View>         
-                <TouchableOpacity>
-                  <Image source={require("./assets/images/MakeKit/product_select.png")} style={{marginTop:windowHeight/8, left: -windowWidth/17}} />
-                </TouchableOpacity>
-            </View>  
-      
-
-          </View>
-        </View>
-
-  </ScrollView>
-)};
-
-return (
-  <SafeAreaView>
-{/*====================================== header ---------------------------------------------------- */}
-    <View style={styles.header}>
-      <Image source={require("./assets/images/MakeKit/tent.png")} style={{marginTop:windowHeight/64}}/>
-        <Text style={styles.header_title}>텐트</Text>
-      
-    </View>
-  <Image source={require("./assets/images/MakeKit/Top_line.png")}/>
-
-{/*---------------------------------------메인 내용1 -------------------------------------------------- */}
-  <Product_info name="돔텐트" info="텐트와 관련된 설명 간단하게 근데 이해하기는 쉽게 그러면 이거는 언제 어케 쓰냐 진심 미치겠네 미치겠네 미치겠네 미치겠네 미치겠네 미치겠네 미치겠네 미치겠네 미치겠네미쳐돌아 돌아 내가 미챠 버려 홀라라랄"/>
-  
-{/*---------------------------------------메인 내용2 -------------------------------------------------- */}
-  <Product_info name="돔텐트" info="텐트와 관련된 설명 간단하게 근데 이해하기는 쉽게 그러면 이거는 언제 어케 쓰냐 진심 미치겠네 미치겠네 미치겠네 미치겠네 미치겠네 미치겠네 미치겠네 미치겠네 미치겠네미쳐돌아 돌아 내가 미챠 버려 홀라라랄"/>
-  
-{/*---------------------------------------메인 내용3 -------------------------------------------------- */}
-  <Product_info name="돔텐트" info="텐트와 관련된 설명 간단하게 근데 이해하기는 쉽게 그러면 이거는 언제 어케 쓰냐 진심 미치겠네 미치겠네 미치겠네 미치겠네 미치겠네 미치겠네 미치겠네 미치겠네 미치겠네미쳐돌아 돌아 내가 미챠 버려 홀라라랄"/>
-  
-{/*--------------------------------------- 하단에 셀렉트 창 -------------------------------------------------- */}
-  <View style={styles.footer_selate}>
-    <Text>내가 선택한 물품</Text>
-      <ScrollView horizontal = {true}> 
-        <Selete_box tent_name="바람막이 텐트" option1="뭘까" option2="정말 뭘까"option3="알 수 없다" money="15000"/>
-        <Selete_box tent_name="바람막이 텐트" option1="뭘까" option2="정말 뭘까"option3="알 수 없다" money="15000"/>
-        
-        </ScrollView>
-  </View>
-
-
-{/*--------------------------------------- footer 캠핑카 ----------------------------------------------------- */}
-<TouchableOpacity  onPress = {() => navigation.navigate("QuizSItuation")}>
-  <Image source={require("./assets/images/MakeKit/next_move_button.png")} style={{left:windowWidth/1.2}}/>
-  </TouchableOpacity>
-  <Image source={require("./assets/images/MakeKit/camping_car.png")} style={{position:'absolute',bottom: -windowHeight/44}}/>
-  <Image source={require("./assets/images/MakeKit/footer.png")} style={{justifyContent: 'flex-end',bottom:-windowHeight/39}}/>
-
-   
-    <StatusBar/>
-  </SafeAreaView>
-)
+  return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="StartPage">
+          
+        <Stack.Screen name="QuizStart" component={QuizStart} options={{ headerShown: false }}/>
+        <Stack.Screen name="QuizSituation" options={{ headerShown: false }} children={
+          ({navigation}) => <QuizSituation navigation={navigation} ansList={ansList}/>
+          }/>
+        <Stack.Screen name= "Quiz1" options={{ headerShown: false }} children={
+          ({navigation}) => <Quiz1 navigation={navigation} ansList={ansList} setansList={setansList}/>
+          }/>
+        <Stack.Screen name="Quiz2" options={{ headerShown: false }}children={
+          ({navigation}) => <Quiz2 navigation={navigation} ansList={ansList} setansList={setansList}/>
+          }/>
+        <Stack.Screen name="Quiz3" options={{ headerShown: false }}children={
+          ({navigation}) => <Quiz3 navigation={navigation} ansList={ansList} setansList={setansList}/>
+          }/>
+        <Stack.Screen name="Quiz4" options={{ headerShown: false }}children={
+          ({navigation}) => <Quiz4 navigation={navigation} ansList={ansList} setansList={setansList}/>
+          }/>
+        <Stack.Screen name="Quiz5" options={{ headerShown: false }}children={
+          ({navigation}) => <Quiz5 navigation={navigation} ansList={ansList} setansList={setansList}/>
+          }/>
+        <Stack.Screen name="Quiz6" options={{ headerShown: false }}children={
+          ({navigation}) => <Quiz6 navigation={navigation} ansList={ansList} setansList={setansList}/>
+          }/>
+        <Stack.Screen name="Quiz7" options={{ headerShown: false }}children={
+          ({navigation}) => <Quiz7 navigation={navigation} ansList={ansList} setansList={setansList}/>
+          }/>
+        <Stack.Screen name="Quiz8" options={{ headerShown: false }}children={
+          ({navigation}) => <Quiz8 navigation={navigation} ansList={ansList} setansList={setansList}/>
+          }/>
+        <Stack.Screen name="Quiz9" options={{ headerShown: false }}children={
+          ({navigation}) => <Quiz9 navigation={navigation} ansList={ansList} setansList={setansList}/>
+          }/>
+        <Stack.Screen name="Quizf" options={{ headerShown: false }}children={
+          ({navigation}) => <Quizf navigation={navigation} ansList={ansList} setansList={setansList}/>
+          }/>
+          <Stack.Screen name="MakeKit" options={{ headerShown: false }}children={
+          ({navigation}) => <MakeKit navigation={navigation} ansList={ansList} />
+          }/>
+        <Stack.Screen name="StartPage" component={StartPage} options={{ headerShown: false }} />
+        <Stack.Screen name="MainPage" component={MainPage} options={{ headerShown: false }}/>
+        </Stack.Navigator>
+        <StatusBar></StatusBar>
+      </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
-  header: {
-    marginTop: windowHeight/45,
-    flexDirection: 'row',
-  },
-  Content_list: {
-    marginRight : windowWidth/50,
-    marginTop: windowHeight/31.4,
-    left: windowWidth/23,
-    flexDirection: 'row',
-  },
-  header_title: {
-    marginTop: windowHeight/14.5,
-    fontSize: 30,
-    textAlign: 'center'
-  },
-  header_content1: {
-    fontSize: 20,
-    textAlign: 'center'
-  },
-  header_content2: {
-    fontSize: 15,
-    textAlign: 'center'
-  },
-  //Content_image: {
-    //marginTop: 20,
-   // borderRadius:50,
-   // backgroundColor: 'tomato',
-  //},
-  Content_explain: {
-    alignItems:'flex-start',
-    height:windowHeight/6,
-    width:windowHeight/3.5,
- //   backgroundColor:'yellow',    
-  },
-  Content_title: {
-    fontSize:20,
-  },
-  slid_button:{
-    position:'absolute',
-    marginTop:windowHeight/7.2,
-    left:-windowWidth/11.5, 
-    height:windowHeight/50
-  },
-  specific_item :{
-    left: windowWidth/100,
-    marginTop: windowHeight/80, 
-    flexDirection: 'row',
-    // backgroundColor:'tomato',
-  },
-  Content: {
-    fontSize:13,
-  },
-  footer_selate: {
-    height:windowHeight/6,
-    //backgroundColor: 'green',
-  },
-  select_box: {
-    marginRight:windowWidth/30,
-    marginTop:-windowHeight/750,
-    left:windowWidth/70,
-    height: windowHeight/8.2,
-    width:windowWidth/1.4,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    borderWidth: 2,
-    borderStyle: 'dashed',
-  },
-  footer_selate_img: {
-    marginTop:windowHeight/130,
-    left: windowWidth/88,
-    height:windowHeight/9.55,
-    width:windowWidth/4.4
-  },
-  picker_value:{
-    borderRadius: 70,
-  },
-  products:{
-    marginLeft: windowWidth/15,
-    flexDirection: 'row',
-    width:windowWidth/3.5,
-    //backgroundColor:"grey",
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'gray'
   },
 });
